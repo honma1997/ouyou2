484ec5cb9e21f5c77e0dedb8d4d3f815f3975e89
EC2 Default User <ec2-user@ip-172-31-9-38.ap-northeast-1.compute.internal>
2025-03-26 08:53:29 +0000

    非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,28 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+  
+    # すでにいいね済みかチェック（これが重要！）
+    unless current_user.favorites.exists?(book_id: @book.id)
+      @favorite = current_user.favorites.create(book_id: @book.id)
+    end
+  
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.js
+    end
   end
-
+  
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: @book.id)
+
+    # nil チェックを追加（エラー回避）
+    @favorite.destroy if @favorite
+
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.js   # 非同期リクエストに対応
+    end
   end
 end
